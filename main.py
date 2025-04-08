83bb803f183133a5e4b3ad2f0ff496b87c7a3b2b
Liz Mendoza <lizeth.mendoza0105@gmail.com>
2025-04-08 14:43:36 -0600

    trying to fix plots for tab 3

--- main.py
+++ main.py
@@ -186,32 +186,25 @@ with tab3:
     yoi = st.text_input('Enter a year')
     plot_female = st.checkbox('Female Plot')
     plot_male = st.checkbox('Male Plot')
-    
-    name_df = df[df['name']==noi]
+
     year_of_interest = yoi
     top_names = df[df['year'] == year_of_interest]
 
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(10,5))
 
 
     if plot_female:
+        category  = "Female"
         top_female = top_names[top_names['sex'] == 'F'].nlargest(10, 'count')
-
-        plt.figure(figsize=(10,5))
         sns.barplot(data=top_female, x='count', y='name')
-        plt.title(f"Top 10 Female Names in {year_of_interest}")
-        plt.xlabel('Count')
-        plt.ylabel('Name')
-        plt.tight_layout()
-        st.pyplot(fig)
 
     if plot_male:
+        category = "Male"
         top_male = top_names[top_names['sex'] == 'M'].nlargest(10, 'count')
-
-        plt.figure(figsize=(10,5))
         sns.barplot(data=top_male, x='count', y='name')
-        plt.title(f"Top 10 Male Names in {year_of_interest}")
-        plt.xlabel('Count')
-        plt.ylabel('Name')
-        plt.tight_layout()
-        st.pyplot(fig)
+
+    plt.title(f"Top 10 {category} Names in {year_of_interest}")
+    plt.xlabel('Count')
+    plt.ylabel('Name')
+    plt.tight_layout()
+    st.pyplot(fig)
